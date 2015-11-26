1e9e55f43a6f121d393fbfe178455f34764ac343
Layl <layl@edgeworksgames.com>
2015-11-26 16:19:48 +0100

    Add period dummy bullet shooting

--- Assets/Scripts/EnemyShootBehavior.cs
+++ Assets/Scripts/EnemyShootBehavior.cs
@@ -1,5 +1,32 @@
 ﻿using UnityEngine;
 
-public class EnemyShootBehavior : MonoBehaviour
+namespace Assets.Scripts
 {
+    public class EnemyShootBehavior : MonoBehaviour
+    {
+        private float _shootCooldown;
+
+        public GameObject BulletPrefab;
+
+        private void Start()
+        {
+            _shootCooldown = Random.Range(0f, 2f);
+        }
+
+        private void Update()
+        {
+            _shootCooldown -= Time.deltaTime;
+
+            if (_shootCooldown <= 0)
+            {
+                var obj = Instantiate(BulletPrefab);
+                obj.transform.position = transform.position;
+                obj.transform.rotation = transform.rotation;
+                var bullet = obj.GetComponent<BulletController>();
+                bullet.Player = GameObject.Find("Player");
+
+                _shootCooldown = 2f;
+            }
+        }
+    }
 }
